e52bd668ecf32e6277040c302aafd03c88bd11bd
Antoha5001 <plantsvzzombi@mail.ru>
2017-11-27 08:10:13 +0700

    sfd

--- app/index.php
+++ app/index.php
@@ -2,20 +2,20 @@
 <html lang="ru">
 <head>
 	<meta name="description" content=""/>
-    <meta name="keywords" content=""/>
-    <meta name="author" content=""/>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="keywords" content=""/>
+	<meta name="author" content=""/>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<meta property="og:image" content="path/to/image.jpg">
-    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+	<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
 	<link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
 	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
 	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<!--    <link rel="stylesheet" type="text/css" href="scss/bootstrap.min.css">-->
-    <title>S&Mittler</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!--    <link rel="stylesheet" type="text/css" href="scss/bootstrap.min.css">-->
+	<title>S&Mittler</title>
 
 	<!-- Chrome, Firefox OS and Opera -->
 	<meta name="theme-color" content="#1a1a1a">
@@ -24,25 +24,44 @@
 	<!-- iOS Safari -->
 	<meta name="apple-mobile-web-app-status-bar-style" content="#1a1a1a">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-<style>	body {opacity: 0;overflow-x: hidden;}
-	html {background-color: #fff;	}
-</style>
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<style>   body {
+			opacity: 0;
+			overflow-x: hidden;
+		}
+
+		html {
+			background-color: #1a1a1a;
+		}
+	</style>
 </head>
-<body>
+<body class="is-home">
 <div id="my-page">
-	<div id="my-header">1</div>
-	<div id="my-content">2</div>
+	<div id="my-header">
+		<header class="site-header" style="background-image: url(images/top-bg.jpg)">
+			<div class="top-line">
+				<a href="#" class="logo"><img src="images/logo-1.svg" alt="Салон красоты"></a>
+				<div class="phone"><i class="fa fa-mobile" aria-hidden="true"></i>+7 (495) 118-94-35</div>
+				<button class="hamburger hamburger--collapse is-active" type="button">
+					<span class="hamburger-box">
+						<span class="hamburger-inner"></span>
+					</span>
+				</button>
+			</div>
+		</header>
+	</div>
+	<div id="my-content">2<i class="fa fa-address-book-o" aria-hidden="true"></i>
+	</div>
 	<div id="my-footer">3</div>
 </div>
 
 <!--<script type="text/javascript" src="script/bootstrap.min.js"></script>-->
-  <link rel="stylesheet" type="text/css" href="css/mystyle.min.css">
+<link rel="stylesheet" type="text/css" href="css/mystyle.min.css">
 <script type="text/javascript" src="script/scripts.min.js"></script>
 <!-- <script>window.jQuery || document.write('<script src="script/jquery.js"><\/script>')</script> -->
 </body>
--- app/script/myscript.js
+++ app/script/myscript.js
@@ -1,5 +1,20 @@
 (function () {
 	window.addEventListener('DOMContentLoaded', function () {
 		// console.log(123);
+		var hamburger = document.getElementsByClassName('hamburger');
+
+		console.log(hamburger[0].classList.contains('is-active'));
+		for(var i =0; i < hamburger.length; i++){
+
+			hamburger[i].addEventListener('click', function () {
+				console.log(this);
+				if(this.classList.contains('is-active') ){
+					this.classList.remove('is-active');
+				} else {
+					this.classList.add('is-active');
+				}
+			});
+
+		}
 	});
 })();
